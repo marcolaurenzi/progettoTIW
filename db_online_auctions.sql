00cc06de2295e6e6d03253fd396bec5b151ae2f7
Marco Laurenzi <marco.laurenzi@mail.polimi.it>
2023-05-11 17:42:15 +0200

    project update

--- db_online_auctions.sql
+++ db_online_auctions.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `db_online_auctions` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `db_online_auctions`;
 -- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: db_online_auctions
@@ -18,40 +16,127 @@ USE `db_online_auctions`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Auctions`
+-- Table structure for table `auction`
 --
 
-DROP TABLE IF EXISTS `Auctions`;
+DROP TABLE IF EXISTS `auction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Auctions` (
-  `auctionId` varchar(45) NOT NULL,
+CREATE TABLE `auction` (
+  `id_code` int NOT NULL,
   `username` varchar(45) NOT NULL,
-  `expiringTime` varchar(45) NOT NULL,
-  `expringDate` varchar(45) NOT NULL,
-  `minimumRise` double NOT NULL,
-  `initialPrice` double NOT NULL,
-  PRIMARY KEY (`auctionId`,`username`)
+  `expiry_date_time` datetime NOT NULL,
+  `minimum_offset` float NOT NULL,
+  PRIMARY KEY (`id_code`,`username`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Auctions`
+-- Dumping data for table `auction`
 --
 
-LOCK TABLES `Auctions` WRITE;
-/*!40000 ALTER TABLE `Auctions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Auctions` ENABLE KEYS */;
+LOCK TABLES `auction` WRITE;
+/*!40000 ALTER TABLE `auction` DISABLE KEYS */;
+INSERT INTO `auction` VALUES (0,'marco','2023-05-19 12:52:00',800),(1,'marco','2023-06-17 13:56:34',890),(2,'marco','2022-05-23 00:00:00',1500),(3,'ante','2023-11-21 00:00:00',30),(7,'marco','2023-06-25 13:21:00',300),(74,'marco','2023-05-21 13:30:00',200),(78,'marco','2023-05-20 13:31:00',2000),(79,'marco','2023-05-27 13:35:00',2100),(80,'marco','2023-05-24 13:37:00',4000),(81,'marco','2023-05-13 14:31:00',345),(82,'marco','2023-06-08 14:32:00',7848700),(83,'marco','2024-10-23 15:33:00',12),(84,'marco','2023-05-31 13:07:00',230),(85,'marco','2025-05-06 14:43:00',234),(86,'marco','2023-05-25 14:45:00',333333),(87,'marco','2023-05-05 14:51:00',123),(88,'marco','2023-05-19 14:53:00',222),(89,'marco','2023-05-05 14:56:00',111),(90,'marco','2023-05-11 14:57:00',21222),(91,'marco','2023-05-24 14:57:00',222),(92,'marco','2023-05-13 15:22:00',2222),(93,'marco','2023-05-28 15:26:00',123),(94,'marco','2023-05-27 15:26:00',122333);
+/*!40000 ALTER TABLE `auction` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Users`
+-- Table structure for table `bid`
 --
 
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `bid`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Users` (
+CREATE TABLE `bid` (
+  `date_time` datetime NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `auction_id` int NOT NULL,
+  `amount` float NOT NULL,
+  `sold` tinyint NOT NULL,
+  PRIMARY KEY (`date_time`,`username`,`auction_id`),
+  KEY `auctionId_idx` (`auction_id`),
+  CONSTRAINT `auctionId` FOREIGN KEY (`auction_id`) REFERENCES `auction` (`id_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bid`
+--
+
+LOCK TABLES `bid` WRITE;
+/*!40000 ALTER TABLE `bid` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contain`
+--
+
+DROP TABLE IF EXISTS `contain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contain` (
+  `auction_id` int NOT NULL,
+  `item_id` int NOT NULL,
+  PRIMARY KEY (`auction_id`,`item_id`),
+  KEY `item_id_idx` (`item_id`),
+  CONSTRAINT `auction_id` FOREIGN KEY (`auction_id`) REFERENCES `auction` (`id_code`) ON UPDATE CASCADE,
+  CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`id_code`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contain`
+--
+
+LOCK TABLES `contain` WRITE;
+/*!40000 ALTER TABLE `contain` DISABLE KEYS */;
+INSERT INTO `contain` VALUES (1,1);
+/*!40000 ALTER TABLE `contain` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id_code` int NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `image` blob,
+  `price` float NOT NULL,
+  `availability` tinyint NOT NULL,
+  PRIMARY KEY (`id_code`,`username`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `username2` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (0,'marco','Sedia','Bellissima sedia',NULL,200,1),(1,'marco','Tavolo','Pazzesco',NULL,400,1),(2,'marco','Computer','Computer bellissimo',NULL,1200,1);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
@@ -60,13 +145,13 @@ CREATE TABLE `Users` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Users`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `Users` WRITE;
-/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES ('ante','ante','via'),('marco','marco','viale blign\\u00EC'),('wiimark10','pallina02','via Carpi 4');
-/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('ante','ante','via'),('marco','marco','via balilla');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -82,4 +167,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-08 19:51:31
+-- Dump completed on 2023-05-11 17:41:26
